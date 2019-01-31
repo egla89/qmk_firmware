30c01a8c4b597bc93a2668096856c2825f7989b0
westfoxtrot <max@cablecardesigns. co>
2019-01-31 18:21:17 +0100

    adjusted prophet

--- keyboards/westfoxtrot/prophet/prophet.h
+++ keyboards/westfoxtrot/prophet/prophet.h
@@ -17,6 +17,8 @@
 
 #include "quantum.h"
 
+#define ___ KC_NO
+
 /* This a shortcut to help you visually see your layout.
  *
  * The first section contains all of the arguments representing the physical
