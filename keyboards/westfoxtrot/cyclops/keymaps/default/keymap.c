9550ddd1ba6df5e5c724b459899c1a1aa445d410
Hannes Egler <hannes.egler@gmail.com>
2019-10-29 15:10:05 +0100

    Update keymap.c
    Fixed L_CTRL

--- keyboards/westfoxtrot/cyclops/keymaps/default/keymap.c
+++ keyboards/westfoxtrot/cyclops/keymaps/default/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,         KC_MNXT,
 		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
 		KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_NO, KC_UP,
-		KC_LSFT, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),          KC_LEFT, KC_DOWN, KC_RGHT),
+		KC_LCTRL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),          KC_LEFT, KC_DOWN, KC_RGHT),
 
 	// Fn1 Layer
 	[1] = LAYOUT(
