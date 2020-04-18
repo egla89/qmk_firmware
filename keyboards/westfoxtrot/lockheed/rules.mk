dd863e8f3ea677bc50596e42a1c5697ca228c1ca
Max <17062872+westfoxtrot@users.noreply.github.com>
2020-04-18 11:42:14 +0100

    updated product_id for 65percent updated lockheed keymap

--- keyboards/westfoxtrot/lockheed/rules.mk
+++ keyboards/westfoxtrot/lockheed/rules.mk
@@ -1,5 +1,5 @@
 # MCU name
-MCU = atmega32u4
+MCU = atmega32u2
 
 # Bootloader selection
 #   Teensy       halfkay
@@ -14,7 +14,7 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no        # Console for debug(+400)
@@ -26,7 +26,7 @@ NKRO_ENABLE = yes            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
-UNICODE_ENABLE = no         # Unicode
+UNICODE_ENABLE = yes         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
